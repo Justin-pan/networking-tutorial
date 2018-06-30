af8077ad2105d55a6cae037034094813091cd07a
jpa87 <jpa87@asb9816-01.csil.sfu.ca>
2018-06-29 18:02:18 -0700

    Initial Commit

---
+++ networking tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  networking tutorial
+//
+//  Created by jpa87 on 6/29/18.
+//  Copyright © 2018 jpa87. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
